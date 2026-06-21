6fe24c85e9afe6957351074dd3fc2d9233899050
RxuqZ256 <155100543+RxuqZ256@users.noreply.github.com>
2026-06-21 19:30:30 +0200

    Load slightly larger briefing card

---
+++ tradelens-briefing-size-v13.js
@@ -0,0 +1,32 @@
+(function(){
+  "use strict";
+  var frame=document.getElementById("app");
+
+  function apply(){
+    try{
+      var doc=frame&&(frame.contentDocument||frame.contentWindow.document);
+      if(!doc||!doc.head)return false;
+      if(!doc.getElementById("tl-briefing-size-v13")){
+        var link=doc.createElement("link");
+        link.id="tl-briefing-size-v13";
+        link.rel="stylesheet";
+        link.href="tradelens-briefing-size-v13.css?v=20260621ag";
+        doc.head.appendChild(link);
+      }
+      return true;
+    }catch(_error){return false;}
+  }
+
+  function start(){
+    var tries=0;
+    var timer=setInterval(function(){
+      tries++;
+      if(apply()||tries>=30)clearInterval(timer);
+    },250);
+  }
+
+  if(frame){
+    frame.addEventListener("load",function(){setTimeout(start,250);});
+    setTimeout(start,700);
+  }
+})();
